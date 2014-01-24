5c8cce69a8e9b7a773e0933aed7b5e603d8db930
Lemuel Santos <lemu@commonplaces.com>
2014-01-24 00:31:45 -0500

    Removing nbproject directory generated by NetBeans

--- bootstrap.php
+++ bootstrap.php
@@ -1,26 +1,26 @@
 <?php
-
-define("NATURAL_ROOT_PATH", dirname(__FILE__));
-if (file_exists(NATURAL_ROOT_PATH . '/bootstrap.dev.php')) {
+  define('NATURAL_ROOT_PATH', dirname(__FILE__));
+  if (file_exists(NATURAL_ROOT_PATH . '/bootstrap.dev.php')) {
     require(NATURAL_ROOT_PATH . '/bootstrap.dev.php');
-} else {
+  }
+  else {
     /* Define system defaults and main configuration */
 
     //SET HIVE INTO DEVELOPMENT MODE
-    //CHANGE TO FALSE WHEN RUNNING 
+    //CHANGE TO FALSE WHEN RUNNING
     //IN PRODUCTION ENVIRONMENT
-    define("NATURAL_DEV_MODE", false);
-    define("ENABLE_COLOR_CHANGE", false);
-    
+    define('NATURAL_DEV_MODE', FALSE);
+    define('ENABLE_COLOR_CHANGE', FALSE);
+
     //SET APPLICATION PATHS
-    define("NATURAL_WEB_ROOT", './');
-    define("TITLE", 'Clean Project');
-    define("NATURAL_ROOT_PATH", dirname(__FILE__));
-    define("NATURAL_LIB_PATH", NATURAL_ROOT_PATH . "/lib/");
-    define("NATURAL_CLASSES_PATH", NATURAL_ROOT_PATH . "/lib/classes/");
-    define("NATURAL_IMAGE_PATH", NATURAL_ROOT_PATH . "/media/images/");
-    define("NATURAL_911WSDL_PATH", NATURAL_ROOT_PATH . "/lib/wsdl/");
-    define("NATURAL_TEMPLATE_PATH", NATURAL_ROOT_PATH . '/themes/moonlight/');
+    define('NATURAL_WEB_ROOT', './');
+    define('TITLE', 'Clean Project');
+    define('NATURAL_ROOT_PATH', dirname(__FILE__));
+    define('NATURAL_LIB_PATH', NATURAL_ROOT_PATH . '/lib/');
+    define('NATURAL_CLASSES_PATH', NATURAL_ROOT_PATH . '/lib/classes/');
+    define('NATURAL_IMAGE_PATH', NATURAL_ROOT_PATH . '/media/images/');
+    define('NATURAL_911WSDL_PATH', NATURAL_ROOT_PATH . '/lib/wsdl/');
+    define('NATURAL_TEMPLATE_PATH', NATURAL_ROOT_PATH . '/themes/moonlight/');
     define('TEMPLATE', 'themes/moonlight/');
     define('PAGER_LIMIT', 25);
 
@@ -28,50 +28,50 @@ if (file_exists(NATURAL_ROOT_PATH . '/bootstrap.dev.php')) {
     require_once('classloader.php');
 
     //SET FORMS/MENUS TABLES
-    define("FORM_TABLE", 'form_parameters');
-    define("FIELD_TABLE", 'field_templates');
-    define("FIELDSET_TABLE", 'fieldsets');
-    define("MAIN_MENU_TABLE", 'main_menu');
-    define("SUB_MENU_TABLE", 'sub_menu');
-    define("SIDE_MENU_TABLE", 'side_menu');
-    define("MODULES_TABLE", 'module');
+    define('FORM_TABLE', 'form_parameters');
+    define('FIELD_TABLE', 'field_templates');
+    define('FIELDSET_TABLE', 'fieldsets');
+    define('MAIN_MENU_TABLE', 'main_menu');
+    define('SUB_MENU_TABLE', 'sub_menu');
+    define('SIDE_MENU_TABLE', 'side_menu');
+    define('MODULES_TABLE', 'module');
 
     //E-MAIL SENDER
-    define("NATURAL_EMAIL_SENDER", "noreply@natural.net");
+    define('NATURAL_EMAIL_SENDER', 'noreply@natural.net');
 
     //WEBSITE DOMAIN
-    define("NATURAL_DOMAIN", "https://www.natural.net");
+    define('NATURAL_DOMAIN', 'https://www.natural.net');
 
     //COMPANY NAME
-    define("NATURAL_COMPANY", "Open Source Mind LLC");
+    define('NATURAL_COMPANY', 'Open Source Mind LLC');
 
     //PLATFORM NAME
-    define("NATURAL_PLATFORM", "Clean Project");
+    define('NATURAL_PLATFORM', 'Clean Project');
 
     //LOAD ERROR MESSAGE LIBRARY
-    require_once(NATURAL_LIB_PATH . "errorcodes.lib.php");
-    require_once(NATURAL_LIB_PATH . "util.func.php");
+    require_once(NATURAL_LIB_PATH . 'errorcodes.lib.php');
+    require_once(NATURAL_LIB_PATH . 'util.func.php');
 
     //Define primary Database Name
-    define("NATURAL_DBNAME", "clean_repo");
+    define('NATURAL_DBNAME', 'clean_repo');
 
     //SET PRODUCTION DATABASE INFORMATION
-    define("NATURAL_DBHOST", '65.99.233.14');
-    define("NATURAL_DBUSER", 'hiveadmin');
-    define("NATURAL_DBPASS", '090909');
+    define('NATURAL_DBHOST', '65.99.233.14');
+    define('NATURAL_DBUSER', 'hiveadmin');
+    define('NATURAL_DBPASS', '090909');
 
     //SET MAGIC KEY
-    define("NATURAL_MAGIC_KEY", "68eKAgHqaS2mY5VCfE1jdPATwEfU5DD7R0nzCJ2cdnhgA32Ym21U");
+    define('NATURAL_MAGIC_KEY', '68eKAgHqaS2mY5VCfE1jdPATwEfU5DD7R0nzCJ2cdnhgA32Ym21U');
 
     //SET API KEY
-    define("NATURAL_API_KEY", "b1076550323b21a1652922e64dfad6d7");
+    define('NATURAL_API_KEY', 'b1076550323b21a1652922e64dfad6d7');
 
-    //SET HIVE REVISION FROM SVN 
-    $svnid = substr("$Rev: 12 $", 6);
+    //SET HIVE REVISION FROM SVN
+    $svnid = substr('$Rev: 12 $', 6);
     $svnid = intval(substr($svnid, 0, strlen($svnid) - 2));
-    define("NATURAL_REVISION", $svnid);
+    define('NATURAL_REVISION', $svnid);
 
     //SET HIVE CURRENT VERSION
-    define("NATURAL_VERSION", "Clean Project 1.0");
-}
+    define('NATURAL_VERSION', 'Clean Project 1.0');
+  }
 ?>
--- index.php
+++ index.php
@@ -1,15 +1,16 @@
 <?
-    require_once('bootstrap.php');
-    require_once(NATURAL_CLASSES_PATH.'datamanager.class.php');
-    require_once(NATURAL_CLASSES_PATH.'forms.class.php');
+  require_once('bootstrap.php');
+  require_once(NATURAL_CLASSES_PATH . 'datamanager.class.php');
+  require_once(NATURAL_CLASSES_PATH . 'forms.class.php');
 
-    $find_company = true;
-    if($find_company){
-        if(isset($_SESSION['logged'])){
-            session_destroy();
-        }
-        require_once(NATURAL_TEMPLATE_PATH . 'login.php');
-    }else{
-        header( 'Location: signup.php' ) ;
+    $find_company = TRUE;
+    if ($find_company) {
+      if (isset($_SESSION['logged'])) {
+        session_destroy();
+      }
+      require_once(NATURAL_TEMPLATE_PATH . 'login.php');
+    }
+    else{
+      header('Location: signup.php');
     }
 ?>
