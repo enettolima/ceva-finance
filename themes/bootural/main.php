f07267e05b330a0266b577fdfdfb228ae74a1db4
Lemuel Santos <lemu@commonplaces.com>
2014-02-09 19:45:46 -0500

    Code formatting

--- themes/bootural/main.php
+++ themes/bootural/main.php
@@ -55,111 +55,93 @@
     <script type="text/javascript" src="<?php print NATURAL_WEB_ROOT ?>lib/js/jqplot.pointLabels.min.js"></script>
     <script type="text/javascript" src="<?php print NATURAL_WEB_ROOT ?>lib/js/jqplot.highlighter.min.js"></script>
     <script type="text/javascript" src="<?php print NATURAL_WEB_ROOT ?>lib/js/jqplot.cursor.min.js"></script>
-
   </head>
 
   <body  class="page-main <?php ($_SESSION['log_interface']) ? print $_SESSION['log_interface']  : print 'green'; ?>">
 
-                                                        <div id="top-header">
-                                                            <span>
-                                                                <?php print $version . ' - ' . $actual_date; ?> - <a href="logout.php" title="Logout" class="logout">Logout</a>
-                                                            </span>
-                                                        </div>
+    <div id="top-header">
+      <span><?php print $version . ' - ' . $actual_date; ?> - <a href="logout.php" title="Logout" class="logout">Logout</a></span>
+    </div>
 
-                                                        <!-- header-wrapper -->
-                                                        <div id="header-wrapper" style="display: <?php print $closed ?>;">
+    <!-- header-wrapper -->
+    <div id="header-wrapper" style="display: <?php print $closed ?>;">
 
-                                                            <!-- header-inner -->
-                                                            <!--  <div id="header-inner" class="clear-block">
-                                                                <div id="logo">
-                                                                </div>
-                                                                <ul id="header-info">
-                                                                  <li><?php print $version ?></li>
-                                                                  <li><?php print $loginname ?></li>
-                                                            <?php if (ENABLE_COLOR_CHANGE): ?>
-                                                                                            <li id="style"><span id="color-actual"></span> <span id="style-info">Style: <?php print $_SESSION['log_interface']; ?></span></li>
-                                                                                            <li id="colors"><a id="blue" class="color-option blue-option" alt="Blue" title="Blue"></a> <a id="gray" class="color-option gray-option" alt="Gray" title="Gray"></a> <a id="green" class="color-option green-option" alt="Green" title="Green"></a> <a id="orange" class="color-option orange-option" alt="Orange" title="Orange"></a> <a id="pink" class="color-option pink-option" alt="Pink" title="Pink"></a></li>
-                                                            <?php endif; ?>
-                                                                </ul>
-                                                              </div>
-                                                            -->
-                                                            <!-- end header-inner
-                                                                <li id="logout"><a href="logout.php" title="Logout">Logout</a></li>
+    <!-- header-inner -->
+    <!--  <div id="header-inner" class="clear-block">
+      <div id="logo"></div>
+        <ul id="header-info">
+          <li><?php print $version ?></li>
+          <li><?php print $loginname ?></li>
+          <?php if (ENABLE_COLOR_CHANGE): ?>
+          <li id="style"><span id="color-actual"></span> <span id="style-info">Style: <?php print $_SESSION['log_interface']; ?></span></li>
+          <li id="colors"><a id="blue" class="color-option blue-option" alt="Blue" title="Blue"></a> <a id="gray" class="color-option gray-option" alt="Gray" title="Gray"></a> <a id="green" class="color-option green-option" alt="Green" title="Green"></a> <a id="orange" class="color-option orange-option" alt="Orange" title="Orange"></a> <a id="pink" class="color-option pink-option" alt="Pink" title="Pink"></a></li>
+          <?php endif; ?>
+        </ul>
+      </div> -->
+      <!-- end header-inner
+      <li id="logout"><a href="logout.php" title="Logout">Logout</a></li>
+      -->
+    </div>
+    <!-- end header -->
 
-                                                            -->
+    <!-- menu-wrapper -->
+    <div id="menu-wrapper">
 
-                                                        </div>
-                                                        <!-- end header -->
+      <!-- menu-inner -->
+      <div id="menu-inner" class="clear-block">
+        <? print $menu ?>
+      </div>
+      <!-- end menu-inner -->
 
-                                                        <!-- menu-wrapper -->
-                                                        <div id="menu-wrapper">
+      <!-- <div id="menu-logo" class="clear-block"></div>-->
+      <!-- end menu-logo -->
 
-                                                            <!-- menu-inner -->
-                                                            <div id="menu-inner" class="clear-block">
-                                                                <? print $menu ?>
-                                                            </div>
-                                                            <!-- end menu-inner -->
+    </div>
+    <!-- end menu-wrapper -->
 
-<!--                                                            <div id="menu-logo" class="clear-block">
+    <div id="account-header">
+      <span id="account-topic"><?php print $account_topic; ?></span>
+      <span id="back-link"><?php print $back_link; ?> </span>
+    </div>
 
-                                                            </div>-->
-                                                            <!-- end menu-logo -->
-
-                                                        </div>
-                                                        <!-- end menu -->
-
-                                                        <div id="account-header">
-                                                            <span id="account-topic"><?php print $account_topic; ?></span>
-                                                            <span id="back-link"><?php print $back_link; ?> </span>
-                                                        </div>
-                                                        <!-- content-wrapper -->
-                                                        <div id="content-wrapper">
-
-                                                            <!-- content-inner -->
-                                                            <div id="content-inner" class="clear-block">
-                                                                <div id="menu-hide-button" class="<?php print $button ?>"></div>
-
-                                                                <div id="status-message"></div>
-
-                                                                <!-- content -->
-                                                                <div id='content'>
-                                                                    <?php print $content; ?>
-                                                                </div>
-                                                                <!-- end content -->
-
-                                                            </div>
-                                                            <!-- end content-inner -->
-
-                                                        </div>
-                                                        <!-- end content -->
+    <!-- content-wrapper -->
+    <div id="content-wrapper">
+      <!-- content-inner -->
+      <div id="content-inner" class="clear-block">
+        <div id="menu-hide-button" class="<?php print $button ?>"></div>
+        <div id="status-message"></div>
+        <!-- content -->
+        <div id='content'>
+          <?php print $content; ?>
+        </div>
+        <!-- end content -->
+      </div>
+      <!-- end content-inner -->
+    </div>
+    <!-- end content -->
 
 
-                                                        <!-- footer-wrapper -->
-                                                        <div id="footer-wrapper">
+    <!-- footer-wrapper -->
+    <div id="footer-wrapper">
+      <!-- footer-inner -->
+      <div id="footer-inner" class="clear-block">
+        <img id="logo-footer" src="<?php print TEMPLATE ?>img/logo-footer.png" />
+      </div>
+      <!-- end footer-inner -->
+    </div>
+    <!-- end footer -->
 
-                                                            <!-- footer-inner -->
-                                                            <div id="footer-inner" class="clear-block">
-                                                                <img id="logo-footer" src="<?php print TEMPLATE ?>img/logo-footer.png" />
-                                                            </div>
-                                                            <!-- end footer-inner -->
+    <!-- loading -->
+    <div id="loading">
+      Loading
+    </div>
+    <!-- end loading -->
 
-                                                        </div>
-                                                        <!-- end footer -->
+    <!-- tooltip -->
+    <div id="tooltip-box"></div>
+    <!-- end tooltip -->
 
-                                                        <!-- loading -->
-                                                        <div id="loading">
-                                                            Loading
-                                                        </div>
-                                                        <!-- end loading -->
-
-                                                        <!-- tooltip -->
-                                                        <div id="tooltip-box">
-                                                        </div>
-                                                        <!-- end tooltip -->
-                                                        <!-- Le javascript
-                                                            ================================================== -->
-                                                        <!-- Placed at the end of the document so the pages load faster -->
-
+    <!-- Placed at the end of the document so the pages load faster -->
     <script type="text/javascript" src="<?php print TEMPLATE ?>js/jquery-1.11.0.min.js"></script>
     <script type="text/javascript" src="<?php print TEMPLATE ?>bootstrap/js/bootstrap.min.js"></script>
   </body>
